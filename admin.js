6223fd26edb62561e6b4b153ce2569e4c7512b72
AA <centralemergenciaslosbronces@gmail.com>
2026-05-25 16:15:38 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -228,7 +228,7 @@ document.getElementById("exportarPartes").onclick = () => {
       + `${p.vehiculo || ""};`
       + `${p.ambulancia || ""};`
       + `${p.servicioMedico || ""};`
-      + `${p.tipoAtencion ? "SI" : "NO"};`
+      + `${p.existeAtencion || ""}
       + `${p.pacientes || "0"};`
       + `${p.clave || ""};`
       + `${p.tipoAtencion || ""};`
