2b83668aefedb455f9bf9976262565cb70850a63
AA <centralemergenciaslosbronces@gmail.com>
2026-06-04 07:38:46 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -60,13 +60,6 @@ if (ultima) {
 
 }
 
-document.getElementById("ultimaEmergencia").innerHTML = `
-  📅 ${ultima.fecha} ⏱️ ${ultima.hora || "--"}<br>
-  📍 ${ultima.lugar || "-"}<br>
-  🚒 ${ultima.tipo || "-"}<br>
-  📝 ${descripcion}
-`;
-
 console.log("📊 Historial:", historial);
 console.log("📋 Partes:", partes);
 
