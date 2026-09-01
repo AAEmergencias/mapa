64360ac9a5a230961d502b267f945ae07309ac22
AA <centralemergenciaslosbronces@gmail.com>
2026-09-01 06:13:38 -0400

    Create usuarios.js

---
+++ usuarios.js
@@ -0,0 +1,53 @@
+import {
+
+db,
+collection,
+getDocs
+
+}
+from "./firebase.js";
+
+const tabla =
+document.getElementById(
+"tablaUsuarios"
+);
+
+async function cargarUsuarios(){
+
+tabla.innerHTML =
+"Cargando...";
+
+const snapshot =
+await getDocs(
+collection(
+db,
+"usuarios"
+));
+
+let html = "";
+
+snapshot.forEach(docSnap => {
+
+const u =
+docSnap.data();
+
+html += `
+<div class="usuario">
+
+<b>${u.nombre}</b><br>
+
+📧 ${u.correo}<br>
+
+👤 ${u.rol}<br>
+
+</div>
+`;
+
+});
+
+tabla.innerHTML =
+html;
+
+}
+
+cargarUsuarios();
