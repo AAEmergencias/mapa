4ac4c46bae10b40a74489a12317b0f6b1400266c
AA <centralemergenciaslosbronces@gmail.com>
2026-09-14 13:18:56 -0300

    Update unidades.js

--- unidades.js
+++ unidades.js
@@ -1,3 +1,94 @@
-console.log(
-  "🚒 Gestión de Unidades cargada"
+import {
+
+  auth,
+  db,
+
+  onAuthStateChanged,
+
+  doc,
+  getDoc,
+
+  collection,
+  getDocs
+
+}
+from "./firebase.js";
+
+onAuthStateChanged(
+  auth,
+  async (user) => {
+
+    if (!user) {
+
+      window.location.href =
+        "login.html";
+
+      return;
+
+    }
+
+    cargarUnidades();
+
+  }
 );
+
+async function cargarUnidades() {
+
+  const lista =
+    document.getElementById(
+      "listaUnidades"
+    );
+
+  lista.innerHTML = "";
+
+  const snapshot =
+    await getDocs(
+      collection(
+        db,
+        "unidades"
+      )
+    );
+
+    snapshot.forEach(
+    docSnap => {
+
+      const data =
+        docSnap.data();
+
+            lista.innerHTML += `
+
+<tr>
+
+  <td>
+    ${data.codigo || "-"}
+  </td>
+
+  <td>
+    ${data.nombre || "-"}
+  </td>
+
+  <td>
+    ${data.base || "-"}
+  </td>
+
+  <td>
+    ${(data.categorias || []).join(", ")}
+  </td>
+
+  <td>
+    ${
+      data.activa
+        ? "✅ Sí"
+        : "❌ No"
+    }
+  </td>
+
+</tr>
+
+`;
+
+    }
+  );
+
+}
+
